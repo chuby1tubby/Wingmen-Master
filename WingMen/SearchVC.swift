49688546c73369e7c504fa4e3e373c7a729a5e09
chuby1tubby <knakamura13@apu.edu>
2016-10-10 00:24:13 -0700

    Login Screen Complete

---
+++ WingMen/SearchVC.swift
@@ -0,0 +1,25 @@
+//
+//  SearchVC.swift
+//  WingMen
+//
+//  Created by Kyle Nakamura on 10/5/16.
+//  Copyright © 2016 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
